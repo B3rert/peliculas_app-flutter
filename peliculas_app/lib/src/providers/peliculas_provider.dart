00667b0e3e2d93aba5080fe6d318e4e55f5f5f77
Robert L <robertmorales181@gmail.com>
2020-10-06 20:07:23 -0600

    App peliculas - seccion, sigue 108

---
+++ peliculas_app/lib/src/providers/peliculas_provider.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+import 'package:peliculas_app/src/models/pelicula_model.dart';
+import 'package:http/http.dart' as http;
+
+class Peliculasprovider {
+  String _apikey = "35bcfabf51a11d03d58fada555490629";
+  String _url = "api.themoviedb.org";
+  String _language = "es-ES";
+
+  Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
+    final resp = await http.get(url);
+    final decodedData = json.decode(resp.body);
+    final peliculas = new Peliculas.fromJsonList(
+      decodedData['results'],
+    );
+
+    return peliculas.items;
+  }
+
+  Future<List<Pelicula>> getEnCines() async {
+    final url = Uri.https(
+      _url,
+      "3/movie/now_playing",
+      {
+        "api_key": _apikey,
+        "language": _language,
+      },
+    );
+
+    return await _procesarRespuesta(url);
+  }
+
+  Future<List<Pelicula>> getPopulares() async {
+    final url = Uri.https(
+      _url,
+      "3/movie/popular",
+      {
+        "api_key": _apikey,
+        "language": _language,
+      },
+    );
+    return await _procesarRespuesta(url);
+  }
+}
